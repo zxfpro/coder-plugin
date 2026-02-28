ed7f729eb8e3d67a66c0fe1772fd32e7f5e943a5
xuefeng <823042332@qq.com>
2026-02-28 15:48:42 +0800

    test1

---
+++ plugins/SampleTDDCoder/scripts/worktreecreate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+say "hello23";
+# INPUT=$(cat); 
+
+# NAME=$(echo "$INPUT" | jq -r .name); 
+# WORKTREE_PATH=\"/tmp/worktrees/$NAME\"; 
+# git worktree add \"$WORKTREE_PATH\" HEAD 
+# ln -sf \"$(pwd)/.env\" \"$WORKTREE_PATH/.env\" 
+# echo \"$WORKTREE_PATH\"
